2ff0667458e41523929db1a661d1d658aa383ff4
Brad <powelbra@oregonstate.edu>
2019-02-23 19:58:14 -0800

    Set up initial files

---
+++ angels.js
@@ -0,0 +1,51 @@
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var handlebars = require('express-handlebars').create({ default: 'main' });
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 2568);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.render('home');
+})
+
+app.get('/pcs', function (req, res) {
+    res.render('pcs');
+})
+
+app.get('/npcs', function (req, res) {
+    res.render('npcs');
+})
+
+app.get('/world', function (req, res) {
+    res.render('world');
+})
+
+app.get('/review', function (req, res) {
+    res.render('review');
+})
+
+app.use(function (req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
