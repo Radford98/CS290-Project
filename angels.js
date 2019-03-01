ed76def8d63261892616c8346ad9bf6812e1ff4f
Brad <powelbra@oregonstate.edu>
2019-02-28 20:55:57 -0800

    NPCs and World
    Created the NPC and World pages. Did some formatting with css. Created a function to request the weather from a real city and assign it to a fake one.

--- angels.js
+++ angels.js
@@ -1,7 +1,8 @@
 
 var express = require('express');
 var app = express();
-var path = require('path');
+var request = require('request');
+var credentials = require('./credentials.js')
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
 var bodyParser = require('body-parser');
 
@@ -10,7 +11,7 @@ app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 2568);
+app.set('port', 2569);
 
 app.use(express.static('public'));
 
@@ -28,9 +29,43 @@ app.get('/npcs', function (req, res) {
     res.render('npcs');
 })
 
+
+
+// Accepts a real city from which to grab the weather and a game city to which to associate that weather.
+function reqWeather(rCity, gCity, context, res, complete) {
+    var ask = 'http://api.openweathermap.org/data/2.5/weather?q=' + rCity + '&APPID=' + credentials.owmKey;
+    request(ask, function (err, response, body) {
+        if (err) {
+            res.write(JSON.stringify(err));
+            res.end();
+        }
+
+        body = JSON.parse(body);    // Convert the body string into a useful object.
+        // The math in the next line is to convert from kelvin to farenheit.
+        context[gCity] = (9 / 5 * (body.main.temp - 273.15) + 32).toFixed(1) + " and " + body.weather[0].main;
+        complete();
+
+    });
+};
+
 app.get('/world', function (req, res) {
-    res.render('world');
-})
+    var context = {};
+    var callbackCount = 0;
+    reqWeather("Salem", "laristal", context, res, complete);
+    reqWeather("Newport", "warsteep", context, res, complete);
+    reqWeather("Seattle", "dostearan", context, res, complete);
+    reqWeather("Denver", "aldurn", context, res, complete);
+    reqWeather("Kansas City", "cartol", context, res, complete);
+
+    // Complete counts the number of times reqWeather is called and renders the page once every city is counted.
+    function complete() {
+        callbackCount++;
+        if (callbackCount >= 5) {
+            res.render('world', context);
+        }
+    }
+});
+
 
 app.get('/review', function (req, res) {
     res.render('review');
